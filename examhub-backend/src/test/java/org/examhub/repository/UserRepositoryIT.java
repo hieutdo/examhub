b4c35c2d50ee912faeae5e22e4db96175304bf44
Hieu Do <mr.hieudt@gmail.com>
2015-05-11 17:49:33 +0700

    Add dbunit and spring-test-dbunit and update UserRepositoryIT

--- examhub-backend/src/test/java/org/examhub/repository/UserRepositoryIT.java
+++ examhub-backend/src/test/java/org/examhub/repository/UserRepositoryIT.java
@@ -1,14 +1,25 @@
 package org.examhub.repository;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import org.examhub.ExamHubApplication;
 import org.examhub.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-import static org.hamcrest.Matchers.notNullValue;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -16,7 +27,17 @@ import static org.junit.Assert.assertThat;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ExamHubApplication.class)
+@TestExecutionListeners({
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class,
+    TransactionalTestExecutionListener.class,
+    DbUnitTestExecutionListener.class})
+@DatabaseSetup(UserRepositoryIT.DATASET)
+@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = { UserRepositoryIT.DATASET })
+@DirtiesContext
 public class UserRepositoryIT {
+
+    protected static final String DATASET = "classpath:datasets/it-users.xml";
 
     @Autowired
     private UserRepository userRepository;
@@ -26,5 +47,20 @@ public class UserRepositoryIT {
         User user = userRepository.findOneByUsername("user1");
 
         assertThat(user, notNullValue());
+    }
+
+    @Test
+    public void findAll_TwoUsersFound_ShouldReturnAListOfTwoUsers() throws Exception {
+        List<User> users = userRepository.findAll();
+
+        assertThat(users.size(), is(1));
+        assertThat(users, contains(
+            allOf(
+                hasProperty("id", is(1L)),
+                hasProperty("username", is("user1")),
+                hasProperty("firstName", is("john")),
+                hasProperty("lastName", is("doe"))
+            )
+        ));
     }
 }
--- examhub-backend/src/test/resources/data.sql
+++
@@ -1,1 +0,0 @@
-insert into exb_users (id, username, password, first_name, last_name, is_activated) VALUES (1, 'user1', '123456', 'fn', 'ln', true);
