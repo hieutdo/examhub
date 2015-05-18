026259e5aa033cb3f510e605f688f1725049ed27
Hieu Do <mr.hieudt@gmail.com>
2015-05-18 14:40:50 +0700

    Move UserDetailsServiceImpl to WebSecurityConfiguration

--- examhub-backend/src/main/java/org/examhub/config/WebSecurityConfiguration.java
+++ examhub-backend/src/main/java/org/examhub/config/WebSecurityConfiguration.java
@@ -1,5 +1,8 @@
 package org.examhub.config;
 
+import org.examhub.domain.UserAccount;
+import org.examhub.repository.UserAccountRepository;
+import org.examhub.web.filter.CsrfCookieGeneratorFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +12,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -23,7 +28,18 @@ import javax.servlet.http.HttpServletResponse;
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserAccountRepository userAccountRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            UserAccount userAccount = userAccountRepository.findByUsernameIgnoreCase(username);
+            if (userAccount == null) {
+                throw new UsernameNotFoundException("Username " + username + " was not found in database");
+            }
+            return userAccount;
+        };
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -31,32 +47,34 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-//                .and()
-//            .addFilterAfter(new CsrfCookieGeneratorFilter(), CsrfFilter.class)
-            .exceptionHandling()
-                .authenticationEntryPoint((request, response, authException) -> {
-                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                })
-                .and()
-            .authorizeRequests()
-                .antMatchers("/api/**").authenticated()
-                .and()
-            .formLogin()
-                .loginProcessingUrl("/api/v1/authenticate")
-                .successHandler((request, response, authentication) -> {
-                    response.setStatus(HttpServletResponse.SC_OK);
-                })
-                .failureHandler((request, response, exception) -> {
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                })
-            .permitAll();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .csrf()
+        .and()
+            .addFilterAfter(new CsrfCookieGeneratorFilter(), CsrfFilter.class)
+            .exceptionHandling()
+            .authenticationEntryPoint((request, response, authException) -> {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            })
+        .and()
+            .authorizeRequests()
+            .antMatchers("/api/**").authenticated()
+        .and()
+            .formLogin()
+            .loginProcessingUrl("/api/v1/authenticate")
+            .successHandler((request, response, authentication) -> {
+                response.setStatus(HttpServletResponse.SC_OK);
+            })
+            .failureHandler((request, response, exception) -> {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            })
+            .permitAll();
+        // @formatter:on
     }
 }
