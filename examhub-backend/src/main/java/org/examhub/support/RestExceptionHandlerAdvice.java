db25203249c4354bff01f35b6624c6999d6f447c
Hieu Do <mr.hieudt@gmail.com>
2015-05-25 08:47:41 +0700

    Refactor code

--- examhub-backend/src/main/java/org/examhub/support/RestExceptionHandlerAdvice.java
+++ examhub-backend/src/main/java/org/examhub/support/RestExceptionHandlerAdvice.java
@@ -1,14 +1,13 @@
-package org.examhub.config;
+package org.examhub.support;
 
 import org.examhub.exception.BaseRestException;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
-import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
@@ -24,71 +23,85 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.multiaction.NoSuchRequestHandlingMethodException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
+ * Global exception handler for all {@code @RestController}, producing JSON error messages for rest api.
+ * This class also extends {@link DefaultErrorAttributes} to add more attributes to Spring Boot's
+ * default error attributes.
+ * <p>The returned JSON has the following information:</p>
+ * <ul>
+ * <li>status - http status code (e.g. 400, 500)</li>
+ * <li>error - error reason (e.g. Bad Request, Internal Server Error)</li>
+ * <li>code - api specific error code (e.g. 400001, 500001)</li>
+ * <li>message - message embedded in thrown exception</li>
+ * <li>developerMessage - technical information for developer calling the api</li>
+ * <li>exception - class name of the root exception</li>
+ * <li>errors - any {@link ObjectError}s from a {@link BindingResult} exception
+ * <li>trace - exception stack trace</li>
+ * <li>path - URL path when the exception was raised</li>
+ * <li>moreInfo - URL to web page describes the error fully and possible solutions to help them resolve it</li>
+ * <li>timestamp - the time that the errors were extracted</li>
+ * </ul>
+ *
  * @author Hieu Do
+ * @see DefaultErrorAttributes
  */
-@ControllerAdvice
-public class ExceptionHandlerConfiguration {
+@ControllerAdvice(annotations = RestController.class)
+public class RestExceptionHandlerAdvice extends DefaultErrorAttributes {
 
-    /**
-     * Add more info to Spring Boot's default errorAttributes. Provides the following attributes when possible:
-     * <ul>
-     * <li>timestamp - The time that the errors were extracted</li>
-     * <li>status - The status code</li>
-     * <li>error - The error reason</li>
-     * <li>exception - The class name of the root exception</li>
-     * <li>message - The exception message</li>
-     * <li>errors - Any {@link ObjectError}s from a {@link BindingResult} exception
-     * <li>trace - The exception stack trace</li>
-     * <li>path - The URL path when the exception was raised</li>
-     * <li>code - Application specific error code</li>
-     * <li>developerMessage - Useful technical information for developer calling the api</li>
-     * <li>moreInfo - Optional URL to web page describes the error fully and possible solutions to help them resolve it</li>
-     * </ul>
-     *
-     * @return an instance of {@code ErrorAttributes}
-     */
-    @Bean
-    public ErrorAttributes errorAttributes() {
-        return new DefaultErrorAttributes() {
-            @Override
-            public Map<String, Object> getErrorAttributes(RequestAttributes requestAttributes, boolean includeStackTrace) {
-                Map<String, Object> errorAttributes = super.getErrorAttributes(requestAttributes, includeStackTrace);
-                Throwable error = getError(requestAttributes);
+    private Map<String, Object> createCustomErrorAttributes(RequestAttributes requestAttributes) {
+        Map<String, Object> errorAttributes = new LinkedHashMap<>();
+        Throwable error = getError(requestAttributes);
 
-                if (error != null && error instanceof BaseRestException) {
-                    BaseRestException exception = (BaseRestException) error;
-                    String code = exception.getCode();
-                    String developerMessage = exception.getDeveloperMessage();
-                    String moreInfo = exception.getMoreInfo();
-                    HttpStatus httpStatus = exception.getHttpStatus();
-                    if (httpStatus != null) {
-                        errorAttributes.put("status", httpStatus.value());
-                        errorAttributes.put("error", httpStatus.getReasonPhrase());
-                        requestAttributes.setAttribute("javax.servlet.error.status_code", httpStatus.value(), RequestAttributes.SCOPE_REQUEST);
-                    }
-                    if (!StringUtils.hasText(code)) {
-                        errorAttributes.put("code", code);
-                    }
-                    if (StringUtils.hasText(developerMessage)) {
-                        errorAttributes.put("developerMessage", exception.getDeveloperMessage());
-                    }
-                    if (StringUtils.hasText(moreInfo)) {
-                        errorAttributes.put("moreInfo", exception.getMoreInfo());
-                    }
-                }
-                return errorAttributes;
+        if (error != null && error instanceof BaseRestException) {
+            BaseRestException exception = (BaseRestException) error;
+            String code = exception.getCode();
+            String developerMessage = exception.getDeveloperMessage();
+            String moreInfo = exception.getMoreInfo();
+            HttpStatus httpStatus = exception.getHttpStatus();
+
+            if (httpStatus != null) {
+                errorAttributes.put("status", httpStatus.value());
+                errorAttributes.put("error", httpStatus.getReasonPhrase());
+                requestAttributes.setAttribute("javax.servlet.error.status_code", httpStatus.value(), RequestAttributes.SCOPE_REQUEST);
             }
-        };
+            if (StringUtils.hasText(code)) {
+                errorAttributes.put("code", code);
+            }
+            if (StringUtils.hasText(developerMessage)) {
+                errorAttributes.put("developerMessage", exception.getDeveloperMessage());
+            }
+            if (StringUtils.hasText(moreInfo)) {
+                errorAttributes.put("moreInfo", exception.getMoreInfo());
+            }
+        }
+
+        return errorAttributes;
+    }
+
+    @Override
+    public Map<String, Object> getErrorAttributes(RequestAttributes requestAttributes, boolean includeStackTrace) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(requestAttributes, includeStackTrace);
+        errorAttributes.putAll(this.createCustomErrorAttributes(requestAttributes));
+        return errorAttributes;
+    }
+
+    private ResponseEntity<Map<String, Object>> handleExceptionInternal(HttpServletRequest request, HttpStatus httpStatus) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        Map<String, Object> body = this.getErrorAttributes(requestAttributes, false);
+        return new ResponseEntity<>(body, httpStatus);
     }
 
     /**
@@ -165,10 +178,10 @@ public class ExceptionHandlerConfiguration {
     /**
      * Send a 500 Internal Server Error in case an exception has no handler matched
      *
-     * @param response the current response
+     * @param request the current request
      */
     @ExceptionHandler({Throwable.class})
-    protected void handleThrowable(HttpServletResponse response) throws IOException {
-        response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    protected ResponseEntity<Map<String, Object>> handleThrowable(HttpServletRequest request) {
+        return handleExceptionInternal(request, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
